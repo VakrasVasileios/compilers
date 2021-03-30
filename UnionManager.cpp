e844455ec6b297ff218ef048ad7b439999b75366
V.Vakras <vasilis_98@hotmail.com>
2021-03-30 23:11:01 +0300

    ADDED/TODO: manager class function implementations

---
+++ UnionManager.cpp
@@ -0,0 +1,21 @@
+#include "UnionManager.h"
+
+void
+UnionManager::RegisterAnonimousFunc() {
+
+}
+
+void
+UnionManager::RegisterFunc() {
+
+}
+
+void
+UnionManager::LookUpVar(char* id) {
+
+}
+
+void
+UnionManager::LookUpFunc(char* id) {
+    
+}
