2217984cf7342bce9301f0170fbe0107f842682a
StefanosSoult <stefanoss1498@gmail.com>
2021-04-23 20:50:15 +0300

    MODIFIED: commented out BlockList class

--- include/BlockList.h
+++ include/BlockList.h
@@ -1,22 +1,22 @@
-#include <list>
-#include <stack>
-#include "Block.h"
+// #include <list>
+// #include <stack>
+// #include "Block.h"
 
-#ifndef BLOCK_LIST
-#define BLOCK_LIST
+// #ifndef BLOCK_LIST
+// #define BLOCK_LIST
 
-class BlockList : public std::list<Block> {
-public:
-    void        ResetVisibility(void);
-    void        SetIDVisibility(const std::string& _id, bool _visi);
+// class BlockList : public std::list<Block> {
+// public:
+//     void        ResetVisibility(void);
+//     void        SetIDVisibility(const std::string& _id, bool _visi);
 
-    void        SetCurrentBlockVisibility(bool _visible);
+//     void        SetCurrentBlockVisibility(bool _visible);
 
-    auto        LookUp(const std::string& _id) -> std::pair<const std::string, Variable>*;
+//     auto        LookUp(const std::string& _id) -> std::pair<const std::string, Variable>*;
  
-    Block*      GetCurrentBlock(void)   { return &*(--end()); }
+//     Block*      GetCurrentBlock(void)   { return &*(--end()); }
 
-    void        logBlockList(void);
-};
+//     void        logBlockList(void);
+// };
 
-#endif
+// #endif
--- src/BlockList.cpp
+++ src/BlockList.cpp
@@ -1,30 +1,30 @@
-#include "../include/BlockList.h"
+// #include "../include/BlockList.h"
 
-void
-BlockList::ResetVisibility(void) {
-    BlockList::GetCurrentBlock()->SetVisibilityAll(true);
-}
+// void
+// BlockList::ResetVisibility(void) {
+//     BlockList::GetCurrentBlock()->SetVisibilityAll(true);
+// }
 
-void
-BlockList::SetCurrentBlockVisibility(bool _visible) {
-    BlockList::GetCurrentBlock()->SetVisibilityAll(_visible);
-}
+// void
+// BlockList::SetCurrentBlockVisibility(bool _visible) {
+//     BlockList::GetCurrentBlock()->SetVisibilityAll(_visible);
+// }
 
-auto
-BlockList::LookUp(const std::string& _id) -> std::pair<const std::string, Variable>* {
-    auto iter = begin();
-    for (; iter != end(); iter++) {
-        auto lkp = (*iter).ids.find(_id);
-        if (lkp != (*iter).ids.end()) {
-            return &*lkp;
-        }
-    }
-    return nullptr;
-}
+// auto
+// BlockList::LookUp(const std::string& _id) -> std::pair<const std::string, Variable>* {
+//     auto iter = begin();
+//     for (; iter != end(); iter++) {
+//         auto lkp = (*iter).ids.find(_id);
+//         if (lkp != (*iter).ids.end()) {
+//             return &*lkp;
+//         }
+//     }
+//     return nullptr;
+// }
 
-void
-BlockList::logBlockList(void) {
-    for (auto iter : (*this)) {
-        iter.logBlock();
-    }
-}
+// void
+// BlockList::logBlockList(void) {
+//     for (auto iter : (*this)) {
+//         iter.logBlock();
+//     }
+// }
