49000061a0a940d2382159b22757853eb5e9c952
StefanosSoult <stefanoss1498@gmail.com>
2021-04-24 10:46:59 +0300

    ADDED: parser manager

---
+++ src/symbol_table/SymbolTable.cpp
@@ -0,0 +1,29 @@
+#include "SymbolTable.h"
+
+// void
+// SymbolTable:: Insert(int scope, SymbolTableEntry entry){
+//     this->blocksMap[scope].top().addSymbolTableEntry(entry);
+// }
+
+// SymbolTableEntry
+// SymbolTable:: LookUp(SymbolTableEntry entry) {
+//     // unsigned int entryScope = entry.getId().getScope();
+//     // for (; entryScope >= 0; entryScope--)
+//     // {
+//     //     std::stack<SymbolTableEntry> temp;
+//     //     while (!this->blocksMap[entryScope].empty())
+//     //     {
+//     //         if (entry != this->blocksMap[entryScope].top().getSymbolTableEntries())
+//     //         {
+//     //             /* code */
+//     //         }
+            
+//     //     }
+        
+//     // }    
+// }
+
+// void
+// SymbolTable:: Hide(int scope) {
+
+// }
