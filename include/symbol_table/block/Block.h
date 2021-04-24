bd159a93ef9fc15875295b068fcabbd3f240a3d8
StefanosSoult <stefanoss1498@gmail.com>
2021-04-24 12:15:52 +0300

    ADDED: Block.h and Block.cpp to the symbol_table file

---
+++ include/symbol_table/block/Block.h
@@ -0,0 +1,43 @@
+#include "../symbol_table_entry/SymbolTableEntry.h"
+#include <list>
+
+#ifndef BLOCK
+#define BLOCK
+
+/*
+Provides a block containing symbol table entries.
+*/
+class Block final {
+    private:
+        std::list<SymbolTableEntry> symbolTableEntries;
+    public:
+        /*
+        The Default Constructor of this Block.
+        */
+        Block() = default;
+        /*
+        The Default Destructor of this Block.
+        */
+        ~Block() = default;
+        /*
+        Returns the list of all symbol table entries of this Block.
+        */
+        std::list<SymbolTableEntry>   getSymbolTableEntries() const;  
+        /*
+        Adds a symbol table entry to this Block.
+        */
+        void                            addSymbolTableEntry(SymbolTableEntry entry);
+};
+
+// struct Block {
+//     std::map<const std::string, Variable>  ids;
+//     Block() = default;
+//     Block(std::map<const std::string, Variable> _ids) : ids(_ids) {};
+
+//     void    SetIDVisibility(const std::string& _id, bool _visible);
+//     void    SetVisibilityAll(bool _visible);
+
+//     void    logBlock(void);
+// };
+
+#endif
