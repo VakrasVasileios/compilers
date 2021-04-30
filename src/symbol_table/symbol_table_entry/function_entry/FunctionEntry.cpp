a8b620e344fec445085717b25a7124e031ee985f
StefanosSoult <stefanoss1498@gmail.com>
2021-04-30 17:55:34 +0300

    MODIFIED: renamed operator << overload for function entry's arguments

--- src/symbol_table/symbol_table_entry/function_entry/FunctionEntry.cpp
+++ src/symbol_table/symbol_table_entry/function_entry/FunctionEntry.cpp
@@ -7,9 +7,5 @@ FunctionEntry:: getFormalArguments() const {
 
 std::ostream&
 operator<<(std::ostream& os, const FunctionEntry functionEntry) {
-    for (FormalVariableEntry formalVariableEntry : functionEntry.getFormalArguments())
-    {
-        os << formalVariableEntry;
-    }
     return os << functionEntry;
 }
