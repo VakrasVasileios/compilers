05e47552a5e36d6c1cebc267ff29670a15a38dca
Vakras.Vasilis <vasilis_98@hotmail.com>
2021-05-28 20:09:52 +0300

    ADDED: ProgramConsts singleton class to store constant values and library functions used

---
+++ include/program_consts.h
@@ -0,0 +1,37 @@
+#ifndef _PROGRAMCONSTS_H_
+#define _PROGRAMCONSTS_H_
+
+#include <vector>
+#include <string>
+#include <map>
+
+class ProgramConsts {
+public:
+    static auto     GetInstance(void) -> ProgramConsts&;
+    static auto     GetInstanceConst(void) -> const ProgramConsts&;
+
+    unsigned        InsertNumber(double num);
+    unsigned        InsertString(const std::string& str);
+    unsigned        InsertLibFunc(const std::string& str);
+
+    double          GetNumber(unsigned index);
+    std::string     GetString(unsigned index);
+    std::string     GetLibFunc(unsigned index);
+
+private:
+    std::vector<double>         number_array;
+    std::vector<std::string>    string_array;
+    std::vector<std::string>    libfunc_array;
+
+    std::map<double, unsigned>        number_map;
+    std::map<std::string, unsigned>   string_map;
+    std::map<std::string, unsigned>   libfunc_map;
+
+    static ProgramConsts singleton;
+
+    ProgramConsts() = default;
+    ProgramConsts(const ProgramConsts&) = delete;
+    ProgramConsts(ProgramConsts&&) = delete;
+};
+
+#endif
