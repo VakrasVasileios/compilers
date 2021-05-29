9540d3b2b9a556ef0b68a37ace34a90a76507baa
StefanosSoult <stefanoss1498@gmail.com>
2021-05-29 17:43:37 +0300

    FIXED: dispatcher array

--- src/opcode_dispatcher.cpp
+++ src/opcode_dispatcher.cpp
@@ -1,9 +1,19 @@
 #include "opcode_dispatcher.h"
 
+/* 
+The dispatch takes place because the generate functions index in this
+array matches each respective opcode enum value, from which each  vm
+instruction is generated.                               
+*/
 Instruction*    
 IopCodeDispatcher::Generate(const Quad* quad){
     PRECONDITION(quad != nullptr);
     return (this->*generators[quad->op])(quad);
+}
+
+Instruction*    
+IopCodeDispatcher::generate_ASSIGN(const Quad* quad) {
+    assert(quad->op == ASSIGN_t);
 }
 
 Instruction*    
@@ -31,24 +41,24 @@ IopCodeDispatcher::generate_MOD(const Quad* quad) {
     assert(quad->op == MOD_t);
 }
 
-Instruction*    
-IopCodeDispatcher::generate_TABLECREATE(const Quad* quad) {
-    assert(quad->op == TABLECREATE_t);
+Instruction*
+IopCodeDispatcher::generate_UMINUS(const Quad* quad) {
+    assert(quad->op == UMINUS_t);
 }
 
 Instruction*    
-IopCodeDispatcher::generate_TABLEGETELEM(const Quad* quad) {
-    assert(quad->op == TABLEGETELEM_t);
+IopCodeDispatcher::generate_AND(const Quad* quad) {
+    assert(quad->op == AND_t);
 }
 
 Instruction*    
-IopCodeDispatcher::generate_TABLESETELEM(const Quad* quad) {
-    assert(quad->op == TABLESETELEM_t);
+IopCodeDispatcher::generate_OR(const Quad* quad) {
+    assert(quad->op == OR_t);
 }
 
 Instruction*    
-IopCodeDispatcher::generate_ASSIGN(const Quad* quad) {
-    assert(quad->op == ASSIGN_t);
+IopCodeDispatcher::generate_NOT(const Quad* quad) {
+    assert(quad->op == NOT_t);
 }
 
 Instruction*    
@@ -61,9 +71,14 @@ IopCodeDispatcher::generate_IF_EQ(const Quad* quad) {
     assert(quad->op == IF_EQ_t);
 }
 
+Instruction*
+IopCodeDispatcher::generate_IF_NOTEQ(const Quad* quad) {
+    assert(quad->op == IF_NOTEQ_t);
+}
+
 Instruction*    
-IopCodeDispatcher::generate_IF_GREATER(const Quad* quad) {
-    assert(quad->op == IF_GREATER_t);
+IopCodeDispatcher::generate_IF_LESSEQ(const Quad* quad) {
+    assert(quad->op == IF_LESSEQ_t);
 }
 
 Instruction*    
@@ -77,23 +92,13 @@ IopCodeDispatcher::generate_IF_LESS(const Quad* quad) {
 }
 
 Instruction*    
-IopCodeDispatcher::generate_IF_LESSEQ(const Quad* quad) {
-    assert(quad->op == IF_LESSEQ_t);
+IopCodeDispatcher::generate_IF_GREATER(const Quad* quad) {
+    assert(quad->op == IF_GREATER_t);
 }
 
 Instruction*    
-IopCodeDispatcher::generate_NOT(const Quad* quad) {
-    assert(quad->op == NOT_t);
-}
-
-Instruction*    
-IopCodeDispatcher::generate_OR(const Quad* quad) {
-    assert(quad->op == OR_t);
-}
-
-Instruction*    
-IopCodeDispatcher::generate_AND(const Quad* quad) {
-    assert(quad->op == AND_t);
+IopCodeDispatcher::generate_CALL(const Quad* quad) {
+    assert(quad->op == CALL_t);
 }
 
 Instruction*    
@@ -102,8 +107,8 @@ IopCodeDispatcher::generate_PARAM(const Quad* quad) {
 }
 
 Instruction*    
-IopCodeDispatcher::generate_CALL(const Quad* quad) {
-    assert(quad->op == CALL_t);
+IopCodeDispatcher::generate_RETURN(const Quad* quad) {
+    assert(quad->op == RET_t);
 }
 
 Instruction*    
@@ -117,11 +122,21 @@ IopCodeDispatcher::generate_FUNCSTART(const Quad* quad) {
 }
 
 Instruction*    
-IopCodeDispatcher::generate_RETURN(const Quad* quad) {
-    assert(quad->op == RET_t);
+IopCodeDispatcher::generate_FUNCEND(const Quad* quad) {
+    assert(quad->op == FUNCEND_t);
 }
 
 Instruction*    
-IopCodeDispatcher::generate_FUNCEND(const Quad* quad) {
-    assert(quad->op == FUNCEND_t);
+IopCodeDispatcher::generate_TABLECREATE(const Quad* quad) {
+    assert(quad->op == TABLECREATE_t);
+}
+
+Instruction*    
+IopCodeDispatcher::generate_TABLEGETELEM(const Quad* quad) {
+    assert(quad->op == TABLEGETELEM_t);
+}
+
+Instruction*    
+IopCodeDispatcher::generate_TABLESETELEM(const Quad* quad) {
+    assert(quad->op == TABLESETELEM_t);
 }
