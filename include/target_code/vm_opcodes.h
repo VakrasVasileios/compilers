fd59c66ff094681e1a09ee9860c93a013ca86343
Vakras.Vasilis <vasilis_98@hotmail.com>
2021-06-03 18:55:51 +0300

    MODIFIED: the order of opcodes in the enum

--- include/target_code/vm_opcodes.h
+++ include/target_code/vm_opcodes.h
@@ -7,26 +7,30 @@ namespace target_code {
      * 
      */
     enum Vmopcode {
-        ASSIGN_VM,
-        ADD_VM,
-        SUB_VM,
-        MUL_VM,
-        DIV_VM,
-        MOD_VM,
         CALLFUNC_VM,
         PUSHARG_VM,
         ENTERFUNC_VM,
         EXITFUNC_VM,
         JUMP_VM,
+        NEWTABLE_VM,
+
+        ADD_VM,
+        SUB_VM,
+        MUL_VM,
+        DIV_VM,
+        MOD_VM,
+
         JEQ_VM,
         JNE_VM,
         JGT_VM,
         JLT_VM,
         JGE_VM,
         JLE_VM,
-        NEWTABLE_VM,
+
         TABLEGETELEM_VM,
         TABLESETELEM_VM,
+
+        ASSIGN_VM,
         NOP_VM
     };
 }
