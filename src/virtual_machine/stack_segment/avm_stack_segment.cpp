5bac32f76866b45002f28fc837b8cd8a81ae44c6
StefanosSoult <stefanoss1498@gmail.com>
2021-06-11 22:24:51 +0300

    NOW FIXED: env functions bug

--- src/virtual_machine/stack_segment/avm_stack_segment.cpp
+++ src/virtual_machine/stack_segment/avm_stack_segment.cpp
@@ -49,7 +49,7 @@ namespace virtual_machine
 
     namespace 
     {
-    bool index_in_bounds(const unsigned int index) {
+    bool index_out_of_bounds(const unsigned int index) {
         assert(util::in_range<int>(index, 0, AVM_STACKSIZE - 1));
         return index < cpu::top;
     }
@@ -59,7 +59,7 @@ namespace virtual_machine
     AvmStackSegment::environment(const target_code::GlobalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
         auto global_vmarg_index = AVM_STACKSIZE - 1 - vmarg.get_value();
-        if (index_in_bounds(global_vmarg_index))
+        if (index_out_of_bounds(global_vmarg_index))
             push(memcell::AvmMemcell());
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));    
         return memcells[global_vmarg_index];
@@ -69,7 +69,7 @@ namespace virtual_machine
     AvmStackSegment::environment(const target_code::LocalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
         auto local_vmarg_index = cpu::topsp - vmarg.get_value();
-        if (index_in_bounds(local_vmarg_index))
+        if (index_out_of_bounds(local_vmarg_index))
             push(memcell::AvmMemcell());
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
         return memcells[local_vmarg_index];
@@ -79,7 +79,7 @@ namespace virtual_machine
     AvmStackSegment::environment(const target_code::FormalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
         auto formal_vmarg_index = cpu::topsp + AVM_STACKENV_SIZE + 1 + vmarg.get_value();
-        if (index_in_bounds(formal_vmarg_index))
+        if (index_out_of_bounds(formal_vmarg_index))
             push(memcell::AvmMemcell());
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));    
         return memcells[formal_vmarg_index];
