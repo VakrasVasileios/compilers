58dd11aa2350a333a1f7cef06b99ac8a733a11cb
StefanosSoult <stefanoss1498@gmail.com>
2021-06-11 22:17:57 +0300

    UPDATED: environment functions for supporting unpushed vmargs

--- src/virtual_machine/stack_segment/avm_stack_segment.cpp
+++ src/virtual_machine/stack_segment/avm_stack_segment.cpp
@@ -47,25 +47,42 @@ namespace virtual_machine
         return memcells[size() - 1 - index];
     }
 
+    namespace 
+    {
+    bool index_in_bounds(const unsigned int index) {
+        assert(util::in_range<int>(index, 0, AVM_STACKSIZE - 1));
+        return index < cpu::top;
+    }
+    }
+
     memcell::AvmMemcell
     AvmStackSegment::environment(const target_code::GlobalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
-        PRECONDITION(!empty());
-        return memcells[AVM_STACKSIZE - 1 - vmarg.get_value()];
+        auto global_vmarg_index = AVM_STACKSIZE - 1 - vmarg.get_value();
+        if (index_in_bounds(global_vmarg_index))
+            push(memcell::AvmMemcell());
+        INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));    
+        return memcells[global_vmarg_index];
     }
 
     memcell::AvmMemcell
     AvmStackSegment::environment(const target_code::LocalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
-        PRECONDITION(!empty());
-        return memcells[cpu::topsp - vmarg.get_value()];
+        auto local_vmarg_index = cpu::topsp - vmarg.get_value();
+        if (index_in_bounds(local_vmarg_index))
+            push(memcell::AvmMemcell());
+        INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
+        return memcells[local_vmarg_index];
     }
 
     memcell::AvmMemcell
     AvmStackSegment::environment(const target_code::FormalVmarg vmarg) {
         INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));
-        PRECONDITION(!empty());
-        return memcells[cpu::topsp + AVM_STACKENV_SIZE + 1 + vmarg.get_value()];
+        auto formal_vmarg_index = cpu::topsp + AVM_STACKENV_SIZE + 1 + vmarg.get_value();
+        if (index_in_bounds(formal_vmarg_index))
+            push(memcell::AvmMemcell());
+        INVARIANT(util::in_range<int>(size(), 0, AVM_STACKSIZE));    
+        return memcells[formal_vmarg_index];
     }
 
     bool        
