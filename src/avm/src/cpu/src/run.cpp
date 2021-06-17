61b0a0471716838452df48354336cfd0d520cb48
StefanosSoult <stefanoss1498@gmail.com>
2021-06-17 13:17:41 +0300

    refactored execute_cycle

--- src/avm/src/cpu/src/run.cpp
+++ src/avm/src/cpu/src/run.cpp
@@ -11,20 +11,20 @@ namespace avm
         namespace
         {
         void execute_cycle() {
-        assert (!signals::execution_finished);
-        if (registers::pc == AVM_ENDING_PC) {
-            signals::execution_finished = true;
-            return;
-        }
-        assert (registers::pc < AVM_ENDING_PC);
-        auto instruction = memory::code_segment[registers::pc];
-        signals::curr_line = instruction->get_src_line();
-        assert (signals::curr_line != 0);
-        unsigned old_pc = registers::pc;
-        execute(instruction);
-        if (registers::pc == old_pc)
-            registers::pc++;
-        }
+            assert (!signals::execution_finished);
+            if (registers::pc == AVM_ENDING_PC) {
+                signals::execution_finished = true;
+                return;
+            }
+            assert (registers::pc < AVM_ENDING_PC);
+            auto instruction = memory::code_segment[registers::pc];
+            signals::curr_line = instruction->get_src_line();
+            assert (signals::curr_line != 0);
+            unsigned old_pc = registers::pc;
+            execute(instruction);
+            if (registers::pc == old_pc)
+                registers::pc++;
+            }
         }
     
     void run() {
