6ddcb6afb83725fba39147e534b33c7d132b1e02
Vakras.Vasilis <vasilis_98@hotmail.com>
2021-06-17 18:37:37 +0300

    ADDED: Visit conditional jump implementations

--- src/avm/src/cpu/src/execute_instruction.cpp
+++ src/avm/src/cpu/src/execute_instruction.cpp
@@ -84,26 +84,48 @@ namespace avm
 
             void VisitJne(target_code::Jne* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                registers::bx = translate_operand(inst->get_arg1(), registers::bx);
+                registers::cx = translate_operand(inst->get_arg2(), registers::cx);
+                exec::execute_jne(registers::ax, registers::bx, registers::cx);
             }
 
             void VisitJgt(target_code::Jgt* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                registers::bx = translate_operand(inst->get_arg1(), registers::bx);
+                registers::cx = translate_operand(inst->get_arg2(), registers::cx);
+                exec::execute_jgt(registers::ax, registers::bx, registers::cx);
             }
 
             void VisitJlt(target_code::Jlt* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                registers::bx = translate_operand(inst->get_arg1(), registers::bx);
+                registers::cx = translate_operand(inst->get_arg2(), registers::cx);
+                exec::execute_jlt(registers::ax, registers::bx, registers::cx);
             }
 
             void VisitJge(target_code::Jge* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                registers::bx = translate_operand(inst->get_arg1(), registers::bx);
+                registers::cx = translate_operand(inst->get_arg2(), registers::cx);
+                exec::execute_jge(registers::ax, registers::bx, registers::cx);
             }
 
             void VisitJle(target_code::Jle* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                registers::bx = translate_operand(inst->get_arg1(), registers::bx);
+                registers::cx = translate_operand(inst->get_arg2(), registers::cx);
+                exec::execute_jle(registers::ax, registers::bx, registers::cx);
             }
 
             void VisitJump(target_code::Jump* inst) const override {
                 assert(inst != nullptr);
+                registers::ax = translate_operand(inst->get_result(), nullptr);
+                exec::execute_jmp(registers::ax);
             }
 
             void VisitCallFunc(target_code::CallFunc* inst) const override {
