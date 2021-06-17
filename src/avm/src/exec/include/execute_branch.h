7499948c78363e182c16101b0fab767b1fc3e3d3
Vakras.Vasilis <vasilis_98@hotmail.com>
2021-06-17 18:35:45 +0300

    ADDED: execute conditional jump function signatures

--- src/avm/src/exec/include/execute_branch.h
+++ src/avm/src/exec/include/execute_branch.h
@@ -8,8 +8,25 @@ namespace avm
 {
     namespace exec
     {
-    void execute_jeq(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
-        memcell::AvmMemcell* arg2);
+        void execute_jeq(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jne(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jgt(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jge(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jlt(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jle(memcell::AvmMemcell* res, memcell::AvmMemcell* arg1, 
+            memcell::AvmMemcell* arg2);
+
+        void execute_jmp(memcell::AvmMemcell* res);
     }
 }
 
